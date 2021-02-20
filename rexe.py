ff5c9661f0890726e2830c01ecf8ab1bdea86ffd
srijan-sivakumar <ssivakum@redhat.com>
2021-02-20 15:51:48 +0530

    Command execution support for multinode
    Added command execution support for following
    scenarios:
    1. All nodes : parallelly
    2. All nodes : sequentially
    
    Fixes: #5
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- rexe.py
+++ rexe.py
@@ -1,5 +1,5 @@
 from os import path
-from threading import Thread
+import concurrent.futures
 import argparse
 import logging
 import logging.handlers
@@ -42,6 +42,7 @@ def is_file_accessible(path, mode='rw+'):
 
 class Rexe:
     def __init__(self, conf_path, command_file_path=""):
+        self.host_generic = ['alls', 'allp']
         self.conf_path = conf_path
         self.command_file_path = command_file_path
         self.parse_conf_file()
@@ -82,7 +83,6 @@ class Rexe:
         
         for node in self.host_list:
             node_ssh_client = paramiko.SSHClient()
-
             node_ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             try:
                 node_ssh_client.connect(hostname=node, username=self.host_user, password=self.host_passwd)
@@ -102,30 +102,54 @@ class Rexe:
             return ret_dict
         stdin, stdout, stderr = self.node_dict[node].exec_command(cmd)
         if stderr.readlines() != []:
-            logger.info(stderr.readlines())
-
+            logger.error(stderr.readlines())
+        ret_dict['node'] = node
+        ret_dict['cmd'] = cmd
         ret_dict['Flag'] = True
         ret_dict['msg'] = stdout.readlines()
+        logger.debug(ret_dict)
         return ret_dict
+
+    def execute_command_multinode(self, node_list, cmd):
+        """
+        Function to execute command in multiple nodes
+        parallely.
+        """
+        ret_val = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers = len(node_list)) as executor:
+            future_exec = {executor.submit(self.execute_command, node, cmd): node for node in node_list}
+            for future_handle in concurrent.futures.as_completed(future_exec):
+                try:
+                    ret_val.append(future_handle.result())
+                except Exception as exc:
+                    print(f"Generated exception : {exc}")
+        logger.info(ret_val)
+        return ret_val
 
     def execute_command_file(self):
         """
         Function to execute the commands provided in the
         command file.
         """
-        if self.command_file_path == "" and not self.connect_flag:
+        if self.command_file_path == "":
             return -1
         for command_node in self.exec_data:
-            if command_node not in self.host_list and command_node != "all":
+            if command_node not in self.host_list and command_node not in self.host_generic:
                 logger.info(f"The command node {command_node} is not in host list.")
                 continue
             commands_list = self.exec_data[command_node]
-            for command_line in commands_list:
-                print(command_line) #for compiling else gives Indentation Error
-                dic = self.execute_command(command_node, command_line)
-                for each in dic['msg']:
-                    print(each)
-        print("Run the whole script.")
+            if command_node == 'allp':
+                # This is for parallel execution of commands in all nodes.
+                for command in commands_list:
+                    self.execute_command_multinode(self.host_list, command)
+            elif command_node == 'alls':
+                # Sequential execution of commands in all nodes.
+                for command in commands_list:
+                    for node in self.host_list:
+                        self.execute_command(node, command)
+            else:
+                for command in commands_list:
+                    self.execute_command(node, command)
 
 if __name__ == "__main__":
     exit_flag = False
