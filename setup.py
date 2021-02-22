fd7aded10ebd8caaa30be968b0b83590054fe3e7
aujjwal-redhat <aujjwal@redhat.com>
2021-02-22 14:37:08 +0530

    Added setup.py in the main directory.
    Package created:
    Name: odinControl
    
    Sub-directory: odinControl
    
    Signed-off-by: aujjwal-redhat <aujjwal@redhat.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='odinControl',
+      version='0.1',
+      description='Remote execution of commands from local machine both in  sequential and parallel',
+      url='https://github.com/srijan-sivakumar/odinControl',
+      author='Srijan Sivakumar',
+      author_email='ssivakum@redhat.com',
+      license='GNU General Public License',
+      packages=['odinControl'],
+      zip_safe=False) 
